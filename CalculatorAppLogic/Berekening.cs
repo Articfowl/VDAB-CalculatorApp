343089a37382f557231d5a39fe73d19aac97837d
Articfowl <40487717+Articfowl@users.noreply.github.com>
2019-07-09 13:26:46 +0200

    Add files via upload

---
+++ CalculatorAppLogic/Berekening.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAppLogic
+{
+    public class Berekening
+    {
+        public string Uitkomst { get; set; }
+        public string Teken { get; set; }
+        public double Getal1 { get; set; }
+        public double Getal2 { get; set; }
+
+        public Berekening(string teken, double getal1, double getal2)
+        {
+            Teken = teken;
+            Getal1 = getal1;
+            Getal2 = getal2;
+        }
+
+        public void Bereken()
+        {
+            switch (Teken)
+            {
+                case "+":
+                    Uitkomst = (Getal1 + Getal2).ToString();
+                    break;
+                case "-":
+                    Uitkomst = (Getal1 - Getal2).ToString();
+                    break;
+                case "×":
+                    Uitkomst = (Getal1 * Getal2).ToString();
+                    break;
+                case "÷":
+                    Uitkomst = (Getal1 / Getal2).ToString();
+                    break;
+                default:
+                    Uitkomst = "ERROR";
+                    break;
+            }
+        }
+    }
+}
---
+++ CalculatorAppUI/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorAppLogic;
+
+namespace CalculatorAppUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var button in Controls.OfType<Button>())
+            {
+                button.Click += Button_Click;
+            }
+        }
+        private bool controle = false;
+        public double Getal1 = 0;
+        public double Getal2 = 0;
+        public string Teken = "";
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if(txtOpgave.Text != "") {
+                Getal2 = Double.Parse(txtCalc.Text);
+                Berekening berekening = new Berekening(Teken, Getal1, Double.Parse(txtCalc.Text));
+                berekening.Bereken();
+                txtCalc.Text = berekening.Uitkomst;
+                txtOpgave.Text = "";
+            }
+            else
+            {
+                Berekening berek = new Berekening(Teken, Double.Parse(txtCalc.Text), Getal2);
+                berek.Bereken();
+                txtCalc.Text = berek.Uitkomst;
+                txtOpgave.Text = "";
+            }
+            
+
+        }
+        private void Button_Click(object sender, EventArgs eventArgs)
+        {
+            switch (((Button)sender).Text)
+            {
+                case "CE":
+                    txtCalc.Text = "";
+                    break;
+                case "C":
+                    txtOpgave.Text = "";
+                    txtCalc.Text = "";
+                    break;
+                case "+/-":
+                    if (controle)
+                    {
+                        txtCalc.Text = "-";
+                    }
+                    else
+                    {
+                        txtCalc.Text = (Convert.ToInt32(txtCalc.Text) * -1).ToString();
+                    }
+
+                    break;
+                case "«":
+                    txtCalc.Text = txtCalc.Text.Substring(0, txtCalc.Text.Length - 1);
+                    break;
+                case "%":
+                    txtCalc.Text = (Getal1 * (Convert.ToDouble(txtCalc.Text) / 100)).ToString();
+                    break;
+                case "=":
+                    break;
+                case "√":
+                    if (txtCalc.Text != "" && !controle)
+                    {
+                        txtCalc.Text = Math.Sqrt(Convert.ToDouble(txtCalc.Text)).ToString();
+                    }
+                    break;
+                case "X²":
+                    if (txtCalc.Text != "" && !controle)
+                    {
+                        txtCalc.Text = Math.Pow(Convert.ToDouble(txtCalc.Text), 2).ToString();
+                    }
+                    break;
+                case "¹/x":
+                    if (txtCalc.Text != "")
+                    {
+                        Berekening berek = new Berekening("÷", 1, Double.Parse(txtCalc.Text));
+                        berek.Bereken();
+                        txtCalc.Text = berek.Uitkomst;
+                    }
+                    break;
+                default:
+                    
+                    break;
+            }
+        }
+
+        private void ButtonNum_Click(object sender, EventArgs e)
+        {
+            if (Char.IsDigit(((Button)sender).Text[0])|| ((Button)sender).Text == ".")
+            {
+                if (controle)
+                {
+                    if (txtCalc.Text == "-")
+                    {
+                        controle = false;
+                    }
+                    else
+                    {
+                        txtCalc.Text = "";
+                        controle = false;
+                    }
+
+                }
+                txtCalc.Text += ((Button)sender).Text;
+            }
+            else
+            {
+                if (txtOpgave.Text != "")
+                {
+                    txtOpgave.Text += txtCalc.Text;
+                    Berekening berekening = new Berekening(Teken, Getal1, Double.Parse(txtCalc.Text));
+                    berekening.Bereken();
+                    txtOpgave.Text += ((Button)sender).Text;
+                    txtCalc.Text = berekening.Uitkomst;
+                    Teken = ((Button)sender).Text;
+                    Getal1 = Convert.ToDouble(berekening.Uitkomst);
+                }
+                else
+                {
+                    Getal1 = Convert.ToDouble(txtCalc.Text);
+                    Teken = ((Button)sender).Text;
+                    txtOpgave.Text += txtCalc.Text + ((Button)sender).Text;
+                }
+                controle = true;
+            }
+        }
+    }
+
+}
